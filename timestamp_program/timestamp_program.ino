de980c5fef438ab179b9bffbffb00b018598d218
lucasquieresopa <lucassalanitro32@gmail.com>
2026-05-26 11:38:13 -0300

    reemplazo de loop con millis en vez de delay

--- timestamp_program/timestamp_program.ino
+++ timestamp_program/timestamp_program.ino
@@ -1,22 +1,30 @@
 // Librerías necesarias
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <LittleFS.h>
 #include <ArduinoJson.h>
 
 
-char ssid_buffer[32];
-char pass_buffer[64];
-char mqtt_buffer[32];
+char wifissid_buffer[32];
+char wifipass_buffer[64];
+char mqttsv_buffer[64];
+char mqttusername_buffer[32];
+char mqttpassword_buffer[32];
 
 // Conexión WiFi y Broker MQTT
-const char* SSID = ssid_buffer;
-const char* PASSWORD = pass_buffer;
-const char* MQTTSERVER = mqtt_buffer;
+const char* WIFISSID = wifissid_buffer;
+const char* WIFIPASSWORD = wifipass_buffer;
+const char* MQTTSERVER = mqttsv_buffer;
+const char* MQTTUSERNAME = mqttusername_buffer;
+const char* MQTTPASSWORD = mqttpassword_buffer;
+const int MQTTPORT = 8883;
+const char* topic_publish = "pdytr/tr";
+
 
 // Cliente MQTT
-WiFiClient espClient;
-PubSubClient client(espClient);
+WiFiClientSecure wifiClient;
+PubSubClient mqttClient(wifiClient);
 
 
 bool cargarCredenciales() {
@@ -47,9 +55,12 @@ bool cargarCredenciales() {
   }
 
   // Asignar los valores del JSON a nuestras variables globales
-  strlcpy(ssid_buffer, doc["ssid"] | "", sizeof(ssid_buffer));
-  strlcpy(pass_buffer, doc["password"] | "", sizeof(pass_buffer));
-  strlcpy(mqtt_buffer, doc["mqtt_server"] | "", sizeof(mqtt_buffer));
+  strlcpy(wifissid_buffer, doc["ssid"] | "", sizeof(wifissid_buffer));
+  strlcpy(wifipass_buffer, doc["password"] | "", sizeof(wifipass_buffer));
+  strlcpy(mqttsv_buffer, doc["mqtt_server"] | "", sizeof(mqttsv_buffer));
+  strlcpy(mqttusername_buffer, doc["mqtt_username"] | "", sizeof(mqttusername_buffer));
+  strlcpy(mqttpassword_buffer, doc["mqtt_password"] | "", sizeof(mqttpassword_buffer));
+
 
   Serial.println("[ÉXITO] Credenciales leídas correctamente de la memoria Flash.");
   return true;
@@ -57,17 +68,17 @@ bool cargarCredenciales() {
 
 // Función para conectar/reconectar a MQTT
 void reconnect() {
-  while (!client.connected()) {
+  while (!mqttClient.connected()) {
     Serial.println(WiFi.localIP());
 
     Serial.print("Intentando conexión MQTT... ");
     
     // Intentamos conectar con un ID único
-    if (client.connect("ESP32Client")) {
+    if (mqttClient.connect("ESP32Client", MQTTUSERNAME, MQTTPASSWORD)) {
       Serial.println("[CONECTADO] Exitosamente al Broker MQTT");
     } else {
       Serial.print("[FALLÓ] Código de error: ");
-      Serial.print(client.state());
+      Serial.print(mqttClient.state());
       Serial.println(" -> Reintentando en 5 segundos...");
       delay(5000);
     }
@@ -84,9 +95,9 @@ void setup() {
   if(cargarCredenciales()){
     // Conexión WiFi
     Serial.print("Conectando a la red WiFi: ");
-    Serial.println(SSID);
+    Serial.println(WIFISSID);
     
-    WiFi.begin(SSID, PASSWORD);
+    WiFi.begin(WIFISSID, WIFIPASSWORD);
     while (WiFi.status() != WL_CONNECTED) {
       delay(500);
       Serial.print("."); // Efecto visual de carga
@@ -97,32 +108,33 @@ void setup() {
   Serial.print("Dirección IP asignada: ");
   Serial.println(WiFi.localIP());
 
+  //wifiClient.setInsecure();
+
   // Configuración del servidor MQTT
-  client.setServer(MQTTSERVER, 1883);
+  mqttClient.setServer(MQTTSERVER, MQTTPORT);
 }
 
 void loop() {
   // Si el ESP32 se desconecta del MQTT, intenta reconectar
-  if (!client.connected()) {
+  if (!mqttClient.connected()) {
     reconnect();
   }
-  client.loop(); // Mantiene viva la conexión y procesa mensajes entrantes
+  mqttClient.loop(); // Mantiene viva la conexión y procesa mensajes entrantes
 
 
-  char msg[100];
-  unsigned long timestamp = millis();
-  sprintf(msg, "{\"tr\": %.2lu}", timestamp);
-  
-  // Intentar publicar el mensaje
-  Serial.print("Enviando datos al tópico 'pdytr/tr': ");
-  Serial.println(msg);
+  long now = millis();
+  if (now - previous_time > 1000) { // Publish every 10 seconds
+    previous_time = now;
 
-  if (client.publish("pdytr/tr", msg)) {
-    Serial.println("[ÉXITO] Mensaje enviado correctamente");
-  } else {
-    Serial.println("[ERROR] No se pudo enviar el mensaje");
-  }
+    unsigned long timestamp = millis();
 
-  Serial.println("-------------------------------------------");
-  delay(1000); // Espera 1 segundos antes del próximo envío
+    char msg_buffer[20]; 
+
+    sprintf(msg_buffer, "%lu", timestamp);
+
+    Serial.print("Timestamp a enviar: ");
+    Serial.println(msg_buffer);
+
+    mqttClient.publish(topic_publish, msg_buffer);
+    }
 }
