21710454b1156efff4c29c117eba807a06cf3453
lucasquieresopa <lucassalanitro32@gmail.com>
2026-06-16 14:24:54 -0300

    time offset is now the median instead of the prom

--- pingpong_timestamp/pingpong_timestamp.ino
+++ pingpong_timestamp/pingpong_timestamp.ino
@@ -17,7 +17,8 @@ const char* WIFIPASSWORD = wifipass_buffer;
 const char* MQTTSERVER   = mqttsv_buffer;
 const char* MQTTUSERNAME = mqttusername_buffer;
 const char* MQTTPASSWORD = mqttpassword_buffer;
-const int   MQTTPORT     = 8883;
+const int   MQTTPORT     = 8883; // para hivemq
+//const int   MQTTPORT     = 1883; // para mosquitto
 
 // --- Topics ---
 const char* TOPIC_DATA     = "pdytr/tr";       // real time messages
@@ -26,12 +27,13 @@ const char* TOPIC_PONG     = "pdytr/pong";     // Node-RED -> ESP32
 
 // --- TLS ---
 String ca_cert_content;
-WiFiClientSecure wifiClient;
+WiFiClientSecure wifiClient; // para hivemq
+//WiFiClient wifiClient; // para mosquitto
 PubSubClient mqttClient(wifiClient);
 
 
-// --- Cristian algorithm variables ---
-const int   CALIBRATION_COUNT    = 10;   // roundtrip count
+// --- Calibration algorithm variables ---
+const int   CALIBRATION_COUNT    = 50;   // roundtrip count
 const float SAMPLE_THRESHOLD    = 0.3f;  // Discard the lowest 30%
 
 volatile bool  waitingPong      = false;
@@ -39,13 +41,17 @@ volatile long long t0Ping       = 0;   // Timestamp of first ping
 long long offsetMs              = 0;   // Offset between ESP and Node-Red
 bool  calibrationReady          = false;
 
-struct CristianSample {
+volatile long long lastCalculatedRTT    = 0;
+volatile long long lastCalculatedOffset = 0;
+
+
+struct CalibrationSample {
   long long rtt;
   long long offset;
 };
 
 
-CristianSample rttSamples[CALIBRATION_COUNT];
+CalibrationSample rttSamples[CALIBRATION_COUNT];
 int receivedSamples             = 0;
 long previousTime               = 0;
 
@@ -70,13 +76,13 @@ void setup() {
 
   timeSynch();
 
-  wifiClient.setCACert(ca_cert_content.c_str());
+  wifiClient.setCACert(ca_cert_content.c_str()); // comentar para mosquitto
   mqttClient.setServer(MQTTSERVER, MQTTPORT);
   mqttClient.setCallback(mqttCallback);  // callback registration
   mqttClient.setBufferSize(256);
 
   reconnect();
-  calibrateCristian();  
+  calibrateRTT();  
 }
 
 
@@ -111,10 +117,20 @@ bool loadCredentials() {
   /* Parsing of credentials */
   strlcpy(wifissid_buffer,    doc["ssid"]          | "", sizeof(wifissid_buffer));
   strlcpy(wifipass_buffer,    doc["password"]       | "", sizeof(wifipass_buffer));
+  
+  /* para hivemq */
+  
   strlcpy(mqttsv_buffer,      doc["mqtt_server"]    | "", sizeof(mqttsv_buffer));
   strlcpy(mqttusername_buffer,doc["mqtt_username"]  | "", sizeof(mqttusername_buffer));
   strlcpy(mqttpassword_buffer,doc["mqtt_password"]  | "", sizeof(mqttpassword_buffer));
   
+  /* para mosquitto */
+  /**/
+  strlcpy(mqttsv_buffer,      doc["mosquitto_server"] , sizeof(mqttsv_buffer));
+  strlcpy(mqttusername_buffer, ""  , sizeof(mqttusername_buffer));
+  strlcpy(mqttpassword_buffer, ""  , sizeof(mqttpassword_buffer));
+  */
+
   Serial.println("[OK] Credentials loaded successfully.");
   return true;
 }
@@ -168,50 +184,73 @@ void reconnect() {
 /// RTT CALCULATION FUNCTIONS ///
 /////////////////////////////////
 
-void calibrateCristian() {
+void calibrateRTT() {
 
-  Serial.println("\n[CRISTIAN] Initializing Cristian calibration...");
-  receivedSamples = 0;
+  Serial.println("\n[CALIBRATION] Initializing RTT calibration...");
   int intents = 0;
+  const int TOTAL_PINGS = 50;
 
+  int validSamplesCount = 0;
   while (receivedSamples < CALIBRATION_COUNT) {
-    if (intents++ > CALIBRATION_COUNT * 3) {
+    if (intents++ > TOTAL_PINGS * 3) {
       Serial.println("[ERROR] Too much calibration timeouts.");
       return;
     }
-    delay(200);
-    if (!doPing()) continue;
+    if (doPing()) {
+      // Ignoramos las primeras 3 muestras de la ráfaga (Precalentamiento de antena)
+      if (intents <= 3) {
+        Serial.printf("  [WARM-UP] Discarding initial sample #%d to wake up Wi-Fi\n", intents);
+        continue;
+      }
+      
+      rttSamples[validSamplesCount].rtt = lastCalculatedRTT;
+      rttSamples[validSamplesCount].offset = lastCalculatedOffset;
+      validSamplesCount++;
+    }
   }
 
   /* Bubble sort algorithm to detect outliers and recalculate offset*/
   for (int i = 0; i < CALIBRATION_COUNT - 1; i++) {
     for (int j = i + 1; j < CALIBRATION_COUNT; j++) {
       if (rttSamples[j].rtt < rttSamples[i].rtt) {
-        CristianSample tmp = rttSamples[i];
+        CalibrationSample tmp = rttSamples[i];
         rttSamples[i] = rttSamples[j];
         rttSamples[j] = tmp;
       }
     }
   }
 
-  /* Discarding samples by threshold */
-  int validCount = (int)(CALIBRATION_COUNT * (1.0f - SAMPLE_THRESHOLD));
-  long long rttMax = rttSamples[validCount - 1].rtt;
+  // En este punto, localSamples[0] contiene el mensaje MÁS RÁPIDO Y PRECISO del sistema.
+  Serial.printf("  [STATS] Best RTT (Cleanest path): %lld ms\n", rttSamples[0].rtt);
+  Serial.printf("  [STATS] Worst RTT accepted: %lld ms\n", rttSamples[CALIBRATION_COUNT - 1].rtt);
+
+  /* 3. APLICAR FILTRO DE MEDIANA SOBRE LAS MEJORES MUESTRAS */
+  // Descartamos el 30% de las muestras con los RTTs más altos (las que se trabaron)
+  int strictSamplesCount = (int)(CALIBRATION_COUNT * (1.0f - SAMPLE_THRESHOLD)); // Ej: 10 * 0.7 = 7 muestras
   
-  Serial.printf("  Minimum RTT: %lld ms | Maximum RTT accepted: %lld ms\n",
-                rttSamples[0].rtt, rttMax);
-
-  /* Recalculate offset with clean samples*/
-
-  long long sumOfValids = 0;
-  for (int i = 0; i < validCount; i++) {
-    sumOfValids += rttSamples[i].offset;
+  // Para encontrar la mediana de los offsets de estas muestras limpias, los ordenamos a ellos
+  long long cleanOffsets[strictSamplesCount];
+  for (int i = 0; i < strictSamplesCount; i++) {
+    cleanOffsets[i] = rttSamples[i].offset;
+  }
+  
+  // Ordenamos los offsets numéricamente
+  for (int i = 0; i < strictSamplesCount - 1; i++) {
+    for (int j = i + 1; j < strictSamplesCount; j++) {
+      if (cleanOffsets[j] < cleanOffsets[i]) {
+        long long tmp = cleanOffsets[i];
+        cleanOffsets[i] = cleanOffsets[j];
+        cleanOffsets[j] = tmp;
+      }
+    }
   }
 
-  offsetMs = sumOfValids / validCount;
+  // Seleccionamos el valor central (Mediana)
+  int middleIndex = strictSamplesCount / 2;
+  offsetMs = cleanOffsets[middleIndex];
 
   calibrationReady = true;
-  Serial.printf("[CRISTIAN] Completed Cristian calibration. Offset = %lld ms\n", offsetMs);
+  Serial.printf("[CALIBRATION] Completed RTT calibration. Offset = %lld ms\n", offsetMs);
 }
 
 
@@ -269,11 +308,14 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
   long long rtt    = t2 - t0Ping;
   long long offset = t1 - t0Ping - rtt / 2;  // Cristian
 
+  lastCalculatedRTT = rtt;
+  lastCalculatedOffset = offset;
+
   rttSamples[receivedSamples].rtt = rtt;
   rttSamples[receivedSamples].offset = offset;
   receivedSamples++;
 
-  Serial.printf("  PONG #%d | RTT: %lld ms | offset parcial: %lld ms\n",
+  Serial.printf("  PONG #%d | RTT: %lld ms | offset percibido: %lld ms\n",
                 receivedSamples, rtt, offset);
 
   waitingPong = false;
